072220e64b3fba087f9d9d4c6bf4644fdc5f43f1
dev-mdz <144072919+dev-mdz@users.noreply.github.com>
2023-10-29 21:56:09 -0300

    Proyecto Curriculum
    Proyecto final armar un curriculum para Academia Ticmas

---
+++ JS/funciones.js
@@ -0,0 +1,28 @@
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length; x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
